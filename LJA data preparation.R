cbf70f68807c3ed00d0689794a7d713203f10c7e
maartjevdkoppel <mvandekoppel@gmail.com>
2021-06-07 09:19:56 +0200

    Rename vars + export dataset

--- LJA data preparation.R
+++ LJA data preparation.R
@@ -1,7 +1,8 @@
 # LINK JONG AMSTERDAM - BUURTANALYSES
-# Last update: 08/05/21
+# Last update: 07/06/21
 
-# Set-up ---------------------------------------------------------------------------------------
+# SET-UP ---------------------------------------------------------------------------------------
+
 rm(list=ls())
 
 library(raster)
@@ -14,20 +15,22 @@ library(stats)
 library(tidyr)
 library(haven)
 
-# Read data ------------------------------------------------------------------------------------
+
+# READ DATA ------------------------------------------------------------------------------------
 
 # Read 'CMAIN177' for KvK data for 2017
-KVKdata <- read_xls("/Users/Maartje/Desktop/LJA/Buurtanalyse/Data/CMAIN177.xls", 
+KVKdata <- read_xls("/Users/Maartje/Desktop/LJA/Buurtanalyse-LJA/Data/CMAIN177.xls", 
             col_names = TRUE)
 
 # Read 'Basisbestand Gebieden Amsterdam' (BBGA) for neighbourhood variables
 # Data can be retrieved from https://data.amsterdam.nl/datasets/G5JpqNbhweXZSw/basisbestand-gebieden-amsterdam-bbga/
-columnnames <- names(read_xlsx("/Users/Maartje/Desktop/LJA/Buurtanalyse/Data/Buurtkenmerken (versie 10-3-21).xlsx", n_max = 0))
+columnnames <- names(read_xlsx("/Users/Maartje/Desktop/LJA/Buurtanalyse-LJA/Data/Buurtkenmerken (versie 10-3-21).xlsx", n_max = 0))
 columntypes <- ifelse(grepl("^[A-Z]", columnnames),"numeric", "guess")
-buurtdata   <- read_xlsx("/Users/Maartje/Desktop/LJA/Buurtanalyse/Data/Buurtkenmerken (versie 10-3-21).xlsx", sheet = 1, col_names = TRUE, col_types = columntypes)
+buurtdata   <- read_xlsx("/Users/Maartje/Desktop/LJA/Buurtanalyse-LJA/Data/Buurtkenmerken (versie 10-3-21).xlsx", sheet = 1, col_names = TRUE, col_types = columntypes)
 
 # Read translation table: postal codes to neighbourhood codes
-koppeltabel <- read_dta("/Users/Maartje/Desktop/LJA/Buurtanalyse/Data/koppeltabel postcode naar buurt.dta")
+koppeltabel <- read_dta("/Users/Maartje/Desktop/LJA/Buurtanalyse-LJA/Data/koppeltabel postcode naar buurt.dta")
+
 
 # KVK DATA - Add dummies -----------------------------------------------------------------------
 
@@ -38,10 +41,11 @@ KVKdata$stichting <- ifelse(KVKdata$RECHTSVORM == 74, 1, 0)
 KVKdata$vereniging <- ifelse(KVKdata$RECHTSVORM == 71 | KVKdata$RECHTSVORM == 72 | KVKdata$RECHTSVORM == 73, 1, 0)
 
 # Dummy for 'leisure society'
-leisure <- read_xlsx("/Users/Maartje/Desktop/LJA/Buurtanalyse/Data/selectiecodesleisureorganisations.xlsx",
+leisure <- read_xlsx("/Users/Maartje/Desktop/LJA/Buurtanalyse-LJA/Data/selectiecodesleisureorganisations.xlsx",
            col_names = TRUE) # read list of KvK codes that designate leisure organisations
 leisure_codes <- leisure$code
 KVKdata$leisure_org <- ifelse(KVKdata$HAKT %in% leisure_codes, 1, 0) 
+
 
 # NEIGHBOURHOOD DATA - Prepare data -------------------------------------------------------------
 
@@ -62,6 +66,7 @@ buurtdata <- buurtdata %>% filter(buurtdata$jaar == 2016)
 
 # Drop observation for 'Z99': not a real neighbourhood
 buurtdata <- buurtdata[buurtdata$gebiedcode15 != "Z99",]
+
 
 # MERGE DATA ------------------------------------------------------------------------------------
 
@@ -90,7 +95,7 @@ KVKdata_buurt <- merge(KVKdata_buurt, KVKdata_leisure, by="bc_code")
 
 # Merge KVKdata_buurt with buurtdata
 buurtdata <- buurtdata %>% rename(bc_code = gebiedcode15)
-data <- merge(buurtdata, KVKdata_buurt, by="bc_code") # M50 is dropped, KVK not available for this neighbourhood
+data <- merge(buurtdata, KVKdata_buurt, by="bc_code") # M50 is dropped, KVK data not available for this neighbourhood
 
 # Variables for organisation density: number of organisations per 1000 inhabitants
 
@@ -98,3 +103,26 @@ data$stichting_density  <- (data$stichting_count  / data$BEVTOTAAL) * 1000
 data$vereniging_density <- (data$vereniging_count / data$BEVTOTAAL) * 1000
 data$leisure_density    <- (data$leisure_count    / data$BEVTOTAAL) * 1000
 
+
+# EXPORT DATA ------------------------------------------------------------------------------------
+
+data <- data %>% rename(bc_n         = gebiednaam)
+data <- data %>% rename(year         = jaar)
+data <- data %>% rename(population   = BEVTOTAAL)
+data <- data %>% rename(imm_Sur      = BEVSUR_P)
+data <- data %>% rename(imm_Ant      = BEVANTIL_P)
+data <- data %>% rename(imm_Tur      = BEVTURK_P)
+data <- data %>% rename(imm_Mar      = BEVMAROK_P)
+data <- data %>% rename(imm_otherNW  = BEVOVNW_P)
+data <- data %>% rename(imm_W        = BEVWEST_P)
+data <- data %>% rename(imm_autoch   = BEVAUTOCH_P)
+data <- data %>% rename(age_0t18     = BEV0_18_P)
+data <- data %>% rename(age_18t26    = BEV18_26_P)
+data <- data %>% rename(age_27t65    = BEV27_65_P)
+data <- data %>% rename(edu_low      = BEVOPLLAAG_P)
+data <- data %>% rename(edu_mid      = BEVOPLMID_P)
+data <- data %>% rename(edu_high     = BEVOPLHOOG_P)
+data <- data %>% rename(age_66plus   = BEV66PLUS_P)
+data <- data %>% rename(unempl       = PREGWERKL_P)
+
+write.csv(data, "/Users//Maartje/Desktop/LJA/Buurtanalyse-LJA/data_buurtanalyse.csv", row.names = FALSE)
